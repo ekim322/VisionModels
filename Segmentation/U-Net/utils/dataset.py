aaecdec6a92f91a57a9a09fcb946532cef5d4ef4
Shubham Kaushal <47340042+ShubhamKaushal15@users.noreply.github.com>
2022-03-15 01:46:52 -0700

    added augmentations to pipeline

--- Segmentation/U-Net/utils/dataset.py
+++ Segmentation/U-Net/utils/dataset.py
@@ -16,8 +16,52 @@ def UNET_DataLoader(img_dir, mask_dir, batch_size, img_size, split_ratio=0):
     If split ratio is 0 - return dataset 
     Else - split train/val
     """
+
+    type1 = [A.Blur(blur_limit = 50, p = 0.15), 
+         A.GaussianBlur(blur_limit=(15, 51), p=0.15), 
+         A.GlassBlur(sigma=1, max_delta=7, iterations=1, p=0.15), 
+         A.HorizontalFlip(p=0.2), 
+         A.MedianBlur(blur_limit=51, p=0.15), 
+         A.MotionBlur(blur_limit = 47, p=0.15)]
+
+    type2 = [A.CLAHE(clip_limit = 10, p = 0.2), 
+            A.Emboss(alpha=(0.5, 0.9), strength=(0.5, 0.9), p = 0.2), 
+            A.Equalize(p = 0.2), 
+            A.InvertImg(p = 0.01), 
+            A.MultiplicativeNoise(multiplier=(1, 3), p=0.2),
+            A.Solarize(p=0.2)]
+
+    type3 = [A.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5, p = 0.2), 
+            A.HueSaturationValue(hue_shift_limit=50, sat_shift_limit=50, val_shift_limit=50, p=0.2), 
+            A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.2), 
+            A.RandomToneCurve(scale = 0.3, p=0.2),
+            A.RGBShift(r_shift_limit=50, g_shift_limit=50, b_shift_limit=50, p=0.2),
+            A.ToGray(p=0.2), 
+            A.ToSepia(p=0.2)]
+
+    type4 = [A.GaussNoise(var_limit=(45.0, 95.0), p = 0.2), 
+            A.ISONoise(color_shift=(0.1, 0.5), intensity=(0.3, 0.7), p = 0.2),
+            A.Sharpen(alpha=(0.4, 0.8), lightness=(0.5, 1.0), p=0.2)]
+
+    type5 = [A.GridDistortion(num_steps=1, distort_limit=1, p=0.1), 
+            A.OpticalDistortion(distort_limit=0.7, shift_limit=1, p=0.1), 
+            A.RandomGridShuffle(grid=(3, 3), p=0.1),
+            A.Superpixels(p=0.05)]
+
+    type6 = [A.RandomFog(fog_coef_lower=0.3, fog_coef_upper=0.5, alpha_coef=0.08, p=0.1), 
+            A.RandomRain(p=0.1), 
+            A.RandomShadow(p=0.1), 
+            A.RandomSnow(p=0.1), 
+            A.RandomSunFlare(p=0.1)]
+
     transform = A.Compose([
-        A.Resize(width=img_size, height=img_size)
+        A.Resize(width=img_size, height=img_size),
+        np.random.choice(type1), 
+        np.random.choice(type2), 
+        np.random.choice(type3),
+        np.random.choice(type4), 
+        np.random.choice(type5), 
+        np.random.choice(type6)
     ])
 
     dataset = UNET_Dataset(img_dir, mask_dir, transform)
@@ -51,7 +95,7 @@ class UNET_Dataset(Dataset):
     def __init__(self, img_dir, mask_dir, transform=None, rgb=True):
         self.img_dir = img_dir
         self.mask_dir = mask_dir
-        self.transfrom = transform
+        self.transform = transform
         self.rgb = rgb
 
         # Find first file extension and set as data type to look for
@@ -97,7 +141,7 @@ class UNET_Dataset(Dataset):
         if len(np.unique(mask))==2:
             mask = mask/np.max(mask)
 
-        augmentations = self.transfrom(image=image, mask=mask)
+        augmentations = self.transform(image=image, mask=mask)
         image = augmentations['image']
         mask = augmentations['mask']
 
